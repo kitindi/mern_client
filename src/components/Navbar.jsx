0d01ca12cd26ec6516f03c1a053a7b35e3414fab
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-04-01 12:01:23 +0300

    sent a verifcation email OTP code successfully

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { userData, backendUrl, setIsLoggedIn, setUserData } = useContext(AppContext);
+
+  // send verification OTP
+
+  const sendEmailVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + "/api/auth/send-verify-otp");
+      if (data.success) {
+        alert(data.message);
+        navigate("/email-verify");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // logout the user
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + "/api/auth/logout");
+      if (data.success) {
+        setIsLoggedIn(false);
+        setUserData(false);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
@@ -10,12 +45,41 @@ const Navbar = () => {
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dev Mates </span>
           </Link>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Link
-              to="/login"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Login
-            </Link>
+            <div>
+              {userData ? (
+                <div className="flex items-center space-x-2">
+                  <div className="relative group">
+                    <p className="w-8 h-8 rounded-full flex items-center justify-center bg-white font-semibold cursor-pointer">
+                      {userData.name[0].toUpperCase()}
+                    </p>
+
+                    <div className="w-48 absolute top-8 z-10 right-0  p-1 bg-white hidden group-hover:block">
+                      <ul className="text-gray-700 flex flex-col gap-2 p-2">
+                        {!userData.isAccountverified && (
+                          <li onClick={sendEmailVerificationOtp} className="cursor-pointer p-1 hover:bg-gray-300">
+                            Verify Account
+                          </li>
+                        )}
+                        <li className="cursor-pointer p-1 hover:bg-gray-300" onClick={logout}>
+                          Log out
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center space-x-2">
+                  <p className="text-white">Hey, Developer</p>
+                  <Link
+                    to="/login"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Login
+                  </Link>
+                </div>
+              )}
+            </div>
+
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,12 +1,32 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  //  to prevent loss of data when making page refrash after user logged in
+
+  axios.defaults.withCredentials = true;
+
+  //  state variable
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(false);
+
+  //   get Authenticate status
+
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
+      if (data.success) {
+        setIsLoggedIn(true);
+        getUserData();
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   //    Get user data
 
@@ -18,6 +38,17 @@ const AppContextProvider = (props) => {
       alert(error.message);
     }
   };
+
+  //   logout user
+
+  //
+
+  useEffect(() => {
+    getAuthState();
+    getUserData;
+  }, []);
+
+  //   initial state values
   const value = {
     backendUrl,
     isLoggedIn,
